b017b6416d0e533479e669864eb1d107a197d039
flyinsky6 <33474037+flyinsky6@users.noreply.github.com>
2024-10-02 13:43:11 +0800

    Web Server.py

--- Code/Web Server.py
+++ Code/Web Server.py
@@ -167,7 +167,7 @@ st.markdown("""
             border-radius: 0.25vw !important;
             border: 0.25vw solid #374673 !important;
             padding: 0.1vw !important;
-            width: 9.5vw;
+            width: 10vw;
             margin-left: 61vw !important;
         }
         .streamlit-expanderContent {
